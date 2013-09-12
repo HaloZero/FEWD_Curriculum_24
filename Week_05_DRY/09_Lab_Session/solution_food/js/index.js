e9040c58f7c2c00a60e3d2ffd2094c06827bb94b
William Neely <william@piecewise.com>
2013-09-12 12:56:00 -0400

    Lessons for arrays, form lessons notes, anonymous functions code

--- Week_05_DRY/09_Lab_Session/solution_food/js/index.js
+++ Week_05_DRY/09_Lab_Session/solution_food/js/index.js
@@ -1,4 +1,4 @@
-var images=["images/food2.jpg","images/food3.jpg","images/food4.jpg","images/food5.jpg","images/food6.jpg","images/food7.jpg",];
+var images=["images/food2.jpg","images/food3.jpg","images/food4.jpg","images/food5.jpg","images/food6.jpg","images/food7.jpg"];
 var i=0;
 var votes=[];
 function previousImage(){
