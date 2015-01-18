c75a2bb8cf8f58b09edaa4488cad2cf2aacd08c2
rohan <halo.zero00@gmail.com>
2015-01-17 20:25:54 -0800

    Modifying starter code assignment

--- Week_06_Intro_jQuery/10_arrays/starter_code/arrays/js/exercise.js
+++ Week_06_Intro_jQuery/10_arrays/starter_code/arrays/js/exercise.js
@@ -1,35 +1,47 @@
 /**
  * Javascript Exercise: Data Types
  * Most of your answers should be stored in variables called q1, q2 etc.. and the variables printed to the console.
- 
+
  	(i.e) console.log("Question 1" + q1)
  */
 
 /**
  * Question 1
- * Create an array of image sources. Use image1.png, image2.png, and image3.png as the array values.
+ * Create an array of image sources. Use the strings 'image1.jpg', 'image2.jpg', and 'image3.jpg' as the array values.
  */
 
+var q1;
 
 /**
  * Question 2
- * Using the array from Question 1, store the first element of the array in variable q2.
+ * Using the array from Question 1, store the first element
+ * of the array in variable q2.
  */
 
+var q2;
 
 /**
  * Question 3
- * Get the length of the first array (number of elements in the array) and store it in variable q3
+ * Get the length of the first array (number of elements in the array)
+ * and store it in variable q3
  */
 
+var q3;
 
 /**
  * Question 4
- *  Using the array from Question 1, store the last element of the array in variable q4. Hint: How can we get the number of elements in the array?
+ *  Using the array from Question 1, store the last element of the array in variable q4.
+ *  Hint: How can we get the number of elements in the array?
  */
 
+var q4;
 
 /**
  * Question 5
- *  Using the variables from questions 2 and 4, write code that would change the src of the first image on a web page to the src stored in q2 and the last image on a web page to the src stored in q4. For extra credit, troubleshoot why the images still don't show up.
+ *  Using the variables from questions 2 and 4, write code that would change
+ *  the src of the first image on a web page to the src stored in q2
+ *  and the last image on a web page to the src stored in q4.
+ *  Extra Credit: Set the second image to to the 2nd element in the array
  */
+
+
--- Week_06_Intro_jQuery/10_arrays/starter_code/arrays/js/runner.js
+++ Week_06_Intro_jQuery/10_arrays/starter_code/arrays/js/runner.js
@@ -5,7 +5,7 @@ function check(num, answer) {
   } else {
 	$("body").append("<p style=\"color:red;\">Question " + num + ": Try Again.</p>");
   }
-  $("body").append("<p>- - - - - - - - - - - - - - - - - - - - - -</p>")
+  $("body").append("<p>- - - - - - - - - - - - - - - - - - - - - -</p>");
 }
 
 function is_empty(obj) {
@@ -18,9 +18,9 @@ function is_empty(obj) {
 }
 
 
-check(1, q1 && q1.length == 3 && q1[0] == 'image1.png' && q1[1] == 'image2.png' && q1[2] == 'image3.png');
-check(2, q2 && q2 == 'image1.png');
+check(1, q1 && q1.length == 3 && q1[0] == 'image1.jpg' && q1[1] == 'image2.jpg' && q1[2] == 'image3.jpg');
+check(2, q2 && q2 == 'image1.jpg');
 check(3, q3 && q3 == 3);
-check(4, q3 && q4 == 'image3.png');
-check(5,$("img").eq(0).attr("src")=="image1.png" && $("img").eq(1).attr("src")=="image3.png");
+check(4, q3 && q4 == 'image3.jpg');
+check(5,$("img").eq(0).attr("src")=="image1.jpg" && $("img").eq(2).attr("src")=="image3.jpg");
 $("body").append("<h3>Checking Complete!</h3>");
