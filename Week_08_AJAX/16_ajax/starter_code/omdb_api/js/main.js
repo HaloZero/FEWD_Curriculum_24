a36692ddd8d3322ed65e8e6c89ffe21a3a305b5c
rohan <halo.zero00@gmail.com>
2015-02-02 17:30:20 -0800

    Adding starter code examples

---
+++ Week_08_AJAX/16_ajax/starter_code/omdb_api/js/main.js
@@ -0,0 +1,8 @@
+// 1) Find me all movies with the word 'Lego'.
+
+// 2) Find me all movies with the word 'Lego' released in 2013.
+
+// 3) Find me all TV series that have the word 'star'
+
+
+// Extra credit: Use the JSON to populate the table with the titles of the movies for each question
