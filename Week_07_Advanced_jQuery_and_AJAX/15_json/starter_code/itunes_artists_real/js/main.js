65e98e44690d9fce6c931b8b0f52c34605606336
rohan <halo.zero00@gmail.com>
2015-02-02 16:35:51 -0800

    Adding modified jquery plugin to make t easier

--- Week_07_Advanced_jQuery_and_AJAX/15_json/starter_code/itunes_artists_real/js/main.js
+++ Week_07_Advanced_jQuery_and_AJAX/15_json/starter_code/itunes_artists_real/js/main.js
@@ -1,10 +1,6 @@
 // use this JSON to fill in the HTML fields!
 // use the example to see what it's supposed to look like
 
-$.ajax({
-  url: "https://itunes.apple.com/search?term=abba",
-  crossDomain: true,
-  dataType: "jsonp"
-}).done(function(musicPlaylist) {
+$.getJSON('https://itunes.apple.com/search?term=abba', function(musicPlaylist) {
   console.log(musicPlaylist);
 });
