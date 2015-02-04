07de60ff7265acf9386019e5370ff13b11ccce79
rohan <halo.zero00@gmail.com>
2015-02-03 19:53:08 -0800

    Fixing up js steps for project

--- Week_08_AJAX/16_ajax_lab/starter_code/itunes_artists_real/js/main.js
+++ Week_08_AJAX/16_ajax_lab/starter_code/itunes_artists_real/js/main.js
@@ -1,17 +1,18 @@
 // Step 1)
-// Take your previous lab solution and copy and paste it here (the entire Javascript)
-// Make sure it works. I've removed the example song so make sure your code didn't change that by accident.
+// Take your previous lab solution and copy and paste it here (the Javascript only)
+// Get that working! I've removed the example song so make sure your code hasn't broken.
+// If you didn't finish the lab, finish it up for Step 1
 
 // Step 2)
-// Make an AJAX request to this url: https://itunes.apple.com/search?term=abba
-// instead of using the json from the prevoius lab musicPlaylist
-// use the results from the AJAX to fill in the HTML
+// Delete the json provided, the entire variable musicPlaylist
+// now instead make an AJAX request to this url: https://itunes.apple.com/search?term=abba
+// use the results from the AJAX request.
 
 // Step 3)
 // Now the hard part. Instead of doing a request to https://itunes.apple.com/search?term=abba
 // I want you to generate the URL for the AJAX request based on the text
 // inside the input element on the page.
-// So if I type in Journey into that input, it should create the url
+// So if I type Journey into that input, it should create the url
 // https://itunes.apple.com/search?term=journey and do an AJAX request to that URL
 
 
